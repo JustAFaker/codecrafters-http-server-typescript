9e3435ad7ed98f4f07fbe293f43dafcd7dc0cf1d
JustAFaker <tiberiu2109@gmail.com>
2024-07-26 01:22:12 +0300

    read header

--- app/main.ts
+++ app/main.ts
@@ -11,13 +11,19 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     
-    const regex = /^GET (\/|\/echo\/([^\/\s]+))(?=\s+HTTP\/1)/;
+    const regex = /(\/|\/user-agent|\/echo\/([^\/\s]+))(?=\s+HTTP\/1)/;
     const match = request.match(regex);
 
     let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+    console.log(match);
     if (match) {
         if (match[1] === '/') {
             response = "HTTP/1.1 200 OK\r\n\r\n"
+        } else if (match[1] === '/user-agent') {
+            const userAgent = request.match(/^User-Agent: (.+?)\s*$/m);
+            if(userAgent) {
+                response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + userAgent[1].length + "\r\n\r\n" + userAgent[1];
+            }
         } else if (match[2]) {
             response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + match[2].length + "\r\n\r\n" + match[2];
         }
